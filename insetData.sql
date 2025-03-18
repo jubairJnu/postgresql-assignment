6d2a0665e9c03335157c9ef4f1654d2b8966e8ea
Abdullah Al Jubair <jubair2810@gmail.com>
2025-03-18 15:42:34 +0600

    Create insetData.sql

---
+++ insetData.sql
@@ -0,0 +1,18 @@
+INSERT INTO students (first_name, last_name, date_of_birth, email, phone_number)
+VALUES
+    ('John', 'Doe', '1999-05-15', 'john.doe@example.com', '+1234567890'),
+    ('Jane', 'Smith', '2001-08-22', 'jane.smith@example.com', '+9876543210'),
+    ('Alice', 'Johnson', '2002-03-10', 'alice.j@example.com', '+1122334455'),
+    ('Bob', 'Brown', '1998-11-30', 'bob.brown@example.com', '+9988776655'),
+    ('Charlie', 'Davis', '2003-07-05', 'charlie.d@example.com', '+5544332211');
+
+
+INSERT INTO courses (course_name, price)
+VALUES
+    ('Introduction to SQL', 2000),
+    ('Web Development', '2500),
+    ('Data Science Basics', 5000),
+    ('Advanced Python', 9000),
+    ('Database Design', 1500);
+
+  
